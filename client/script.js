f66398f683bf3c5e24f7040cd4161311d92426d3
Stero tECH <agyeisterogh@gmail.com>
2022-12-29 12:34:56 +0000

    Initial commit

---
+++ client/script.js
@@ -0,0 +1,107 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+let loadInterval;
+
+function loader(element) {
+  element.textContent = 'Loading';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+    if (element.textContent === 'Loading....') {
+      element.textContent = 'Loading';
+    }
+  }, 300);
+}
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+function generateuniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAI, value, uniqueId) {
+  return `
+    <div class="wrapper ${isAI && 'ai'}">
+      <div class='chat'>
+        <div class='profile'>
+          <img
+            src='${isAI ? bot : user}'
+            alt="${isAI ? 'bot' : 'user'} avatar"
+          />
+        </div>
+        <div class="message" id="${uniqueId}">${value}</div>
+      </div>
+    </div>
+    `;
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  // user's Chat stripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+  form.reset();
+
+  // bot's ChatStripe
+  const uniqueID = generateuniqueId();
+  chatContainer.innerHTML += chatStripe(true, ' ', uniqueID);
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueID);
+  loader(messageDiv);
+
+  //fetch response from server -> bot response
+  const response = await fetch('http://127.0.0.1:5000', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt'),
+    }),
+  });
+
+  // clear loader
+  clearInterval(loadInterval);
+
+  messageDiv.innerHTML = '';
+
+  if (response.ok) {
+    const data = await response.json();
+    const parseData = data.bot.trim();
+
+    typeText(messageDiv, parseData);
+  } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = 'Something Went Wrong!';
+    alert(err);
+  }
+};
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    handleSubmit(e);
+  }
+});
